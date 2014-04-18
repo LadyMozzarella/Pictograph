b980aa86e67fc6e5a806b79346e3fdb760cbe441
Brittany Mazza <bmazza555@gmail.com>
2014-04-18 16:03:38 -0700

    Add gitignore file

--- js/default.js
+++ js/default.js
@@ -1,3 +1,5 @@
 $(document).ready(function() {
 
+	//login with instagram button needs to go here:
+	window.open('https://instagram.com/oauth/authorize/?client_id=CLIENT_ID&redirect_uri=https://www.brittanymazza.com/pictograph&response_type=token');
 });

843c58d4b4bb9984912d571d895dfac400dc97ce
Brittany Mazza <bmazza555@gmail.com>
2014-04-18 23:17:31 -0700

    Add basic View and Controller functions for signing in with instagram  *Setup Binder/View/Controller in javascript for the basic functionality  *A user can now login with instagram and see the correct information  *Temprorary content added for testing purposes

--- js/default.js
+++ js/default.js
@@ -1,14 +1,60 @@
 $(document).ready(function() {
-	var controller = new pictographController();
-
-	event.preventDefault();
-	$("button").bind('click', function(){controller.loginToInstagram()});
+	var controller = new PictographController();
+  var accessTokenHash = window.location.hash;
+	
+	controller.delegateSetup(accessTokenHash);
 });
 
-function pictographController() {}
+function Binder(controller) {
+	this.controller = controller;
+};
 
-pictographController.prototype = {
+Binder.prototype = {
+	instagramLogin: function() {
+		var that = this;
+		$("button").bind('click', function() {
+			event.preventDefault();
+			that.controller.loginToInstagram()
+		});
+	}
+};
+
+function PictographController() {
+	this.view = new PictographView();
+	this.bind = new Binder(this);
+};
+
+PictographController.prototype = {
 	loginToInstagram: function() {
 		window.open('https://instagram.com/oauth/authorize/?client_id=9e8cb302b04d4fd1be775c062799a962&redirect_uri=http://brittanymazza.com/pictograph/&response_type=token', '_self');
+	},
+	setupInstagramLogin: function() {
+		this.view.showLoginButton();
+		this.bind.instagramLogin();
+	},
+	populateInstagramData: function() {
+		this.view.showContent();
+	},
+	delegateSetup: function(accessTokenHash) {
+		if (accessTokenHash.length == 0) {
+			this.setupInstagramLogin();
+		} 
+		else {
+			this.accessToken = window.location.hash.split('=')[1];
+			this.populateInstagramData();
+		};
 	}
-}
+};
+
+function PictographView() {};
+
+PictographView.prototype = {
+	showLoginButton: function() {
+		$('.login_btn').show();
+		$('.content').hide();
+	},
+	showContent: function() {
+		$('.login_btn').hide();
+		$('.content').show();
+	},
+};
